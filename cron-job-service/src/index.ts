61cecf614971bce544914da1517896a8e820eaf0
RANDRIAMILASOA Stanislas <hajaniainastanislas@gmail.com>
2019-07-19 10:19:30 +0300

    Show log to indicate service status

--- cron-job-service/src/index.ts
+++ cron-job-service/src/index.ts
@@ -4,7 +4,7 @@ import cron from 'cron';
 const CronJob = cron.CronJob;
 const timeZone = 'Africa/Nairobi';
 const cronTime = '00 00 18 * * *';
-
+console.log('Cron started');
 const job = new CronJob(
   cronTime,
   function() {
